9617ca6e134e2e2eca0cbeb92f4a4c5ff71dfaef
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-11-09 20:43:09 -0300

    chore: addComponentInputCalculationsOnScrennPGC

--- src/screens/calculations/pgc/index.tsx
+++ src/screens/calculations/pgc/index.tsx
@@ -5,12 +5,17 @@ import {
   ButtonContainer,
   Container,
   ContainerAge,
+  ContainerCalculaters,
+  ContainerInputsdoubles,
+  ContainerSkinFolds,
+  Content,
 } from "./styles"
 import { InputCalculations } from "../../../components/Forms/InputCalculations"
 import * as Yup from "yup"
 import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import { ButtonComponent } from "../../../components/ButtonComponent"
+import { Input } from "../../../components/Forms/Input"
 
 export function CalculationPgc() {
   const schema = Yup.object().shape({
@@ -32,22 +37,66 @@ export function CalculationPgc() {
   return (
     <Container>
       <BackgroundContent>
-        <ContainerAge>
-          <InputCalculations
-            name="idade"
-            isActive={true}
-            placeholder="Anos"
-            control={control}
-            errorInput={errors.idade && errors.idade.message}
-          />
-        </ContainerAge>
-        <ButtonContainer>
-          <ButtonComponent
-            title={"Calcular"}
-            nameIcon="chevron-right"
-            onPress={handleSubmit(handleCalculate)}
-          />
-        </ButtonContainer>
+        <Content>
+          <ContainerCalculaters>
+            <ContainerAge>
+              <InputCalculations
+                name="idade"
+                TitleCalculate="Idade"
+                isActive={true}
+                control={control}
+                errorInput={errors.idade && errors.idade.message}
+              />
+            </ContainerAge>
+            <ContainerSkinFolds>
+              <ContainerInputsdoubles>
+                <InputCalculations
+                  name="triceps"
+                  TitleCalculate="Tríceps"
+                  isActive={true}
+                  control={control}
+                  errorInput={errors.idade && errors.idade.message}
+                />
+              </ContainerInputsdoubles>
+              <ContainerInputsdoubles>
+                <InputCalculations
+                  name="biceps"
+                  TitleCalculate="Bíceps"
+                  isActive={true}
+                  control={control}
+                  errorInput={errors.idade && errors.idade.message}
+                />
+              </ContainerInputsdoubles>
+            </ContainerSkinFolds>
+            <ContainerSkinFolds>
+              <ContainerInputsdoubles>
+                <InputCalculations
+                  name="subescapular"
+                  TitleCalculate="subescapular"
+                  isActive={true}
+                  control={control}
+                  errorInput={errors.idade && errors.idade.message}
+                />
+              </ContainerInputsdoubles>
+              <ContainerInputsdoubles>
+                <InputCalculations
+                  name="SupraIliaca"
+                  TitleCalculate="Supra Íliaca"
+                  isActive={true}
+                  control={control}
+                  errorInput={errors.idade && errors.idade.message}
+                />
+              </ContainerInputsdoubles>
+            </ContainerSkinFolds>
+          </ContainerCalculaters>
+          <ButtonContainer>
+            <ButtonComponent
+              title={"Calcular"}
+              nameIcon="chevron-right"
+              onPress={handleSubmit(handleCalculate)}
+            />
+          </ButtonContainer>
+        </Content>
       </BackgroundContent>
     </Container>
   )
--- src/screens/calculations/pgc/styles.ts
+++ src/screens/calculations/pgc/styles.ts
@@ -1,25 +1,41 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import styled from 'styled-components/native';
-import { BackgroundComponent } from '../../../components/Background';
-import { Platform, View } from 'react-native';
-import { RFValue } from 'react-native-responsive-fontsize';
-
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import styled from "styled-components/native"
+import { BackgroundComponent } from "../../../components/Background"
+import { Platform, View } from "react-native"
+import { RFValue } from "react-native-responsive-fontsize"
 
 export const Container = styled(GestureHandlerRootView)`
- flex: 1;
-`;
-
-export const BackgroundContent = styled(BackgroundComponent)`
-  justify-content: space-between;
+  flex: 1;
 `
 
+export const BackgroundContent = styled(BackgroundComponent)``
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 ${RFValue(16)}px;
+`
 export const ContainerAge = styled(View)`
+  align-self: center;
+  width: 100%;
+  margin-bottom: ${RFValue(20)}px;
   margin-top: 250px;
 `
+export const ContainerSkinFolds = styled(View)`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: ${RFValue(20)}px;
+`
 
+export const ContainerCalculaters = styled.View``
+
+export const ContainerInputsdoubles = styled.View`
+  width: ${RFValue(130)}px;
+`
 
 export const ButtonContainer = styled.View`
   width: 100%;
-  padding: 0px ${RFValue(20)}px;
-  margin-bottom: ${() => Platform.OS === 'android' ? '16px' : '0px'};
+  padding: ${RFValue(20)}px;
+  margin-bottom: ${() => (Platform.OS === "android" ? "16px" : "0px")};
 `
