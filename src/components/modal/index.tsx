083564be7c77c4ce9cb2c38c19aaf0c5cb4aae66
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-11-23 18:44:34 -0300

    Feat: Create-Modal-feedback

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import { Modal, Text, View, StyleSheet, TouchableOpacity } from "react-native"
+import { Feather } from "@expo/vector-icons"
+
+export function ConfirmationModal({ isVisible, closeModal }) {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={() => {
+        closeModal()
+      }}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Feather name="check-circle" size={50} color="green" />
+          <Text style={styles.successText}>Cadastro concluído com sucesso</Text>
+          <TouchableOpacity style={styles.okButton} onPress={closeModal}>
+            <Text style={styles.okButtonText}>OK</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Fundo escurecido
+  },
+  modalContent: {
+    width: 375,
+    height: 207,
+    backgroundColor: "white",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  successText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 10,
+    textAlign: "center",
+  },
+  okButton: {
+    backgroundColor: "green",
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  okButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+})
