6066163e7cae459b8f6ead38fd85cd04d72882b0
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-11-23 18:46:21 -0300

    Feat: Intergration-with-firebase

--- src/screens/register/nutritionists/index.tsx
+++ src/screens/register/nutritionists/index.tsx
@@ -1,25 +1,34 @@
-import React from "react"
-import {
-  Container,
-  BackgroundContent,
-  ContainerForm,
-  ButtonContainer,
-  Content,
-  ContainerLogo,
-} from "./styles"
-import Logo2 from "../../../assets/Logo2.svg"
-import * as Yup from "yup"
 import { yupResolver } from "@hookform/resolvers/yup"
-import { InputForm } from "../../../components/InputForm"
-import { ButtonComponent } from "../../../components/ButtonComponent"
-import { useForm } from "react-hook-form"
+import auth from "@react-native-firebase/auth"
+import firestore from "@react-native-firebase/firestore"
 import { useNavigation } from "@react-navigation/native"
+import React, { useState } from "react"
+import { useForm } from "react-hook-form"
+import { ActivityIndicator, Alert, Modal, StyleSheet, View } from "react-native"
+import uuid from "react-native-uuid"
+import * as Yup from "yup"
+import Logo2 from "../../../assets/Logo2.svg"
+import { ButtonComponent } from "../../../components/ButtonComponent"
+import { InputForm } from "../../../components/InputForm"
+import { NutritionistProps } from "./props"
+import {
+  BackgroundContent,
+  ButtonContainer,
+  Container,
+  ContainerForm,
+  ContainerLogo,
+  Content,
+} from "./styles"
+import { ConfirmationModal } from "../../../components/modal"
 
 export function RegisterNutritionists() {
   const { navigate } = useNavigation<any>()
+  const [nutritionist, setNutritionist] = useState<NutritionistProps>()
+  const [loading, setLoading] = useState(false)
+  const [isModalVisible, setIsModalVisible] = useState(false)
 
   const schema = Yup.object().shape({
-    name: Yup.string().required("digite seu nome"),
+    firstName: Yup.string().required("digite seu nome"),
     lastName: Yup.string().required("digite seu sobrenome"),
     cpf: Yup.string().required("digite seu CPF"),
     email: Yup.string()
@@ -31,11 +40,6 @@ export function RegisterNutritionists() {
       .oneOf([Yup.ref("password")], "As senhas não coincidem "),
   })
 
-  const handleRegister = () => {
-    navigate("SignIn")
-    console.log("Nutricionista cadastrado com sucesso")
-  }
-
   const {
     control,
     handleSubmit,
@@ -44,6 +48,61 @@ export function RegisterNutritionists() {
   } = useForm({
     resolver: yupResolver(schema),
   })
+
+  async function handleCreateNutritionist({
+    firstName,
+    lastName,
+    cpf,
+    email,
+    password,
+  }: NutritionistProps) {
+    try {
+      setLoading(true) 
+      const emailSnapshot = await firestore()
+        .collection("nutritionist")
+        .where("email", "==", email)
+        .get()
+
+      if (!emailSnapshot.empty) {
+        setLoading(false)
+        Alert.alert("Email já cadastrado")
+      } else {
+        const RegisterNutritionist =
+          await auth().createUserWithEmailAndPassword(email, password)
+
+        if (RegisterNutritionist) {
+          await firestore()
+            .collection("nutritionist")
+            .add({
+              id: String(uuid.v4()),
+              firstName,
+              lastName,
+              cpf,
+              email,
+              password,
+            })
+          setIsModalVisible(true)
+          setLoading(false) 
+        }
+      }
+    } catch (error) {
+      setLoading(false)
+
+      if (error.code === "auth/email-already-in-use") {
+        Alert.alert("Email já cadastrado")
+      } else {
+        Alert.alert("Ocorreu um erro ao processar o cadastro")
+        console.error("Erro:", error)
+      }
+    }
+  }
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+    navigate('SignIn');
+  };
+
+
   return (
     <Container>
       <BackgroundContent>
@@ -55,19 +114,19 @@ export function RegisterNutritionists() {
             <InputForm
               type="custom"
               options={{
-                mask: "AAA",
+                mask: "AAAAAAAAAAAAAAAAAAAAAAAAA",
               }}
-              name="name"
+              name="firstName"
               control={control}
               placeholder={"Digite seu nome"}
-              errorInput={errors.name && errors.name.message}
+              errorInput={errors.firstName && errors.firstName.message}
             />
           </ContainerForm>
           <ContainerForm>
             <InputForm
               type="custom"
               options={{
-                mask: "AAA",
+                mask: "AAAAAAAAAAAAAAAAAAAAAAAAAAA",
               }}
               name="lastName"
               control={control}
@@ -88,7 +147,7 @@ export function RegisterNutritionists() {
             <InputForm
               type="custom"
               options={{
-                mask: "*",
+                mask: "**************************************",
               }}
               name="email"
               control={control}
@@ -101,7 +160,7 @@ export function RegisterNutritionists() {
               name="password"
               type="custom"
               options={{
-                mask: "*",
+                mask: "************",
               }}
               control={control}
               placeholder={"Digite sua senha"}
@@ -113,7 +172,7 @@ export function RegisterNutritionists() {
               name="passwordConfirmation"
               type="custom"
               options={{
-                mask: "*",
+                mask: "************",
               }}
               control={control}
               placeholder={"Confirme sua senha"}
@@ -129,11 +188,42 @@ export function RegisterNutritionists() {
               type="default"
               title={"Cadastar"}
               nameIcon="chevron-right"
-              onPress={handleSubmit(handleRegister)}
+              onPress={handleSubmit(handleCreateNutritionist)}
             />
           </ButtonContainer>
         </Content>
       </BackgroundContent>
+      {loading && (
+        <Modal transparent={true} animationType="fade" visible={loading}>
+          <Modal transparent={true} animationType="fade" visible={loading}>
+            <View style={styles.modalContainer}>
+              <View style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color="green" />
+              </View>
+            </View>
+          </Modal>
+        </Modal>
+      )}
+      <ConfirmationModal isVisible={isModalVisible} closeModal={closeModal} />
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loadingContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+  },
+})
